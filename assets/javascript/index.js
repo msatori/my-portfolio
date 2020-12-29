5f8b4f784cec93914af0c7e76b1b8b34f3e6ce32
Maya Santiago <69778090+msatori@users.noreply.github.com>
2020-12-29 16:05:09 -0600

    scratching all current design

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,4 +1,7 @@
-// ------------- VARIABLES ------------- //
+// ------------- IMPORTS------------- //
+
+
+// ------------- VARIABLES  FOR SCROLLING------------- //
 var ticking = false;
 var isFirefox = (/Firefox/i.test(navigator.userAgent));
 var isIe = (/MSIE/i.test(navigator.userAgent)) || (/Trident.*rv\:11\./i.test(navigator.userAgent));
@@ -6,6 +9,12 @@ var scrollSensitivitySetting = 30; //Increase/decrease this number to change sen
 var slideDurationSetting = 600; //Amount of time for which slide is "locked"
 var currentSlideNumber = 0;
 var totalSlideNumber = $(".background").length;
+
+// ------------- VARIABLES  FOR CARDS------------- //
+
+var card = document.querySelector(".card");
+var cardImage = document.querySelector(".card-img");
+var cardTitle= document.querySelector(".card-title");
 
 // ------------- DETERMINE DELTA/SCROLL DIRECTION ------------- //
 function parallaxScroll(evt) {
@@ -63,3 +72,62 @@ function previousItem() {
   var $currentSlide = $(".background").eq(currentSlideNumber);
   $currentSlide.removeClass("down-scroll").addClass("up-scroll");
 }
+
+
+
+function enterCard() {
+
+  anime.remove(cardImage);
+  anime({
+    targets: cardImage,
+    easing: 'linear',
+    translateY: '-65px',
+    scaleX: '1.5',
+    scaleY: '1.3',
+    duration: 200
+  });
+
+  anime.remove(cardTitle);
+  anime({
+    targets: cardTitle,
+    translateX: '-55',
+    color: [
+      {value: '#fff'}
+    ],
+    easing: 'linear',
+    duration: 200,
+    complete: function() {
+      card5.classList.add('active');
+    }
+  });
+
+}
+
+function leaveCard() {
+
+  anime.remove(cardImage);
+  anime({
+    targets: cardImage,
+    translateY: '0',
+    scaleX: '1',
+    scaleY: '1',
+    elasticity: 100
+  });
+
+  anime.remove(cardTitle);
+  anime({
+    targets: cardTitle,
+    translateX: '0',
+    color: [
+      {value: '#000'}
+    ],
+    elasticity: 100
+  });
+
+  card5.classList.remove('active');
+
+}
+
+
+card5.addEventListener('mouseenter', enterCard, false);
+card5.addEventListener('mouseleave', leaveCard. false);
